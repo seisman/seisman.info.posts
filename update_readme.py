0f5637e57c71fa7d5c8af60068b08a44fb4ed397
Dongdong Tian <seisman.info@gmail.com>
2016-08-01 16:36:35 +0800

    add script

---
+++ update_readme.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+
+f = open("README.md", "w")
+f.write("# SeisMan 博客\n\n")
+
+for year in range(2013, 2017, 1):
+    f.write("## {} 年\n\n".format(year))
+    for md in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
+        date = '-'.join(os.path.basename(md).split('-')[0:3])
+        title = open(md, "r").readlines()[1][7:].strip(' \n"\'')
+        f.write("- {0}: [{1}](/{2}) \n".format(date, title, md))
+    f.write("\n")
+
+f.close()
