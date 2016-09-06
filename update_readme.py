f3315036ff509cec97c8399ca4fa87a50ba5485e
Dongdong Tian <seisman.info@gmail.com>
2016-09-06 22:40:14 +0800

    updates

--- update_readme.py
+++ update_readme.py
@@ -16,3 +16,15 @@ for year in range(2013, 2017, 1):
     f.write("\n")
 
 f.close()
+
+f = open("_pdfs/0.博文目录.txt", "w")
+f.write("# SeisMan 博客博文目录\n\n")
+f.write("请根据博文发布时间寻找对应的PDF文件。\n\n")
+
+for year in range(2013, 2017, 1):
+    f.write("## {} 年\n\n".format(year))
+    for md in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
+        date = '-'.join(os.path.basename(md).split('-')[0:3])
+        title = open(md, "r").readlines()[1][7:].strip(' \n"\'')
+        f.write("- {0}: {1} \n".format(date, title))
+    f.write("\n")
