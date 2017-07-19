b44c51c041a2750d6e23ec472e0b83d1bc2d588f
Dongdong Tian <seisman.info@gmail.com>
2017-07-19 16:43:45 +0800

    简化代码

--- update_readme.py
+++ update_readme.py
@@ -3,28 +3,22 @@
 
 import os
 import glob
+from datetime import datetime
 
-f = open("README.md", "w")
-f.write("# SeisMan 博客\n\n")
 
-for year in range(2013, 2018, 1):
-    f.write("## {} 年\n\n".format(year))
-    for md in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
-        date = '-'.join(os.path.basename(md).split('-')[0:3])
-        title = open(md, "r").readlines()[1][7:].strip(' \n"\'')
-        f.write("- {0}: [{1}](/{2}) \n".format(date, title, md))
-    f.write("\n")
+with open("README.md", "w") as md, open("_pdfs/0.博文目录.txt", "w") as txt:
+    # header
+    md.write("# SeisMan 博客\n\n")
+    txt.write("# SeisMan 博客博文目录\n\n")
+    txt.write("请根据博文发布时间寻找对应的PDF文件。\n\n")
 
-f.close()
-
-f = open("_pdfs/0.博文目录.txt", "w")
-f.write("# SeisMan 博客博文目录\n\n")
-f.write("请根据博文发布时间寻找对应的PDF文件。\n\n")
-
-for year in range(2013, 2017, 1):
-    f.write("## {} 年\n\n".format(year))
-    for md in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
-        date = '-'.join(os.path.basename(md).split('-')[0:3])
-        title = open(md, "r").readlines()[1][7:].strip(' \n"\'')
-        f.write("- {0}: {1}\n".format(date, title))
-    f.write("\n")
+    for year in range(2013, datetime.now().year+1):
+        md.write("## {} 年\n\n".format(year))
+        txt.write("## {} 年\n\n".format(year))
+        for file in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
+            date = '-'.join(os.path.basename(file).split('-')[0:3])
+            title = open(file, "r").readlines()[1][7:].strip(' \n"\'')
+            md.write("- {0}: [{1}](/{2}) \n".format(date, title, file))
+            txt.write("- {0}: {1}\n".format(date, title))
+        md.write("\n")
+        txt.write("\n")
