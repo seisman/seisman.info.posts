383557f3e0ab8f700b3046c1882a0339448df435
Dongdong Tian <seisman.info@gmail.com>
2018-02-02 16:50:00 +0800

    update README

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -6,19 +6,15 @@ import glob
 from datetime import datetime
 
 
-with open("README.md", "w") as md, open("_pdfs/0.博文目录.txt", "w") as txt:
+os.chdir("..")
+with open("README.md", "w") as md:
     # header
     md.write("# SeisMan 博客\n\n")
-    txt.write("# SeisMan 博客博文目录\n\n")
-    txt.write("请根据博文发布时间寻找对应的PDF文件。\n\n")
 
     for year in range(2013, datetime.now().year+1):
         md.write("## {} 年\n\n".format(year))
-        txt.write("## {} 年\n\n".format(year))
-        for file in sorted(glob.glob("_posts/{}-*-*-*.md".format(year))):
+        for file in sorted(glob.glob("post/{}-*-*-*.md".format(year))):
             date = '-'.join(os.path.basename(file).split('-')[0:3])
             title = open(file, "r").readlines()[1][7:].strip(' \n"\'')
             md.write("- {0}: [{1}](/{2}) \n".format(date, title, file))
-            txt.write("- {0}: {1}\n".format(date, title))
         md.write("\n")
-        txt.write("\n")
